16d66364de4b04775f9dbdf28e69d24c4c1fe26a
José P. <shzexyz@gmail.com>
2026-05-25 18:15:26 -0300

    test(money): characterize normalization of trailing fractional zeroes

--- src/test/java/io/github/phlmth/ledgerpay/domain/money/MoneyTest.java
+++ src/test/java/io/github/phlmth/ledgerpay/domain/money/MoneyTest.java
@@ -8,37 +8,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 class MoneyTest {
     @Test
     void shouldCreateMoneyWithTwoDecimalPlaces() {
-        var expectedAmount = new BigDecimal("10.50");
+        var expectedAmount = new BigDecimal("9.50");
 
-        Money money = Money.of("10.50");
+        Money money = Money.of("9.50");
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
     @Test
     void shouldNormalizeValueWithoutDecimalPlaces() {
-        var expectedAmount = new BigDecimal("10.00");
-        
-        Money money = Money.of("10");
+        var expectedAmount = new BigDecimal("9.00");
+
+        Money money = Money.of("9");
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
     @Test
     void shouldNormalizeAmountCreatedThroughCanonicalConstructor() {
-        var expectedAmount = new BigDecimal("10.00");
+        var expectedAmount = new BigDecimal("9.00");
 
-        Money money = new Money(new BigDecimal("10"));
+        Money money = new Money(new BigDecimal("9"));
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
 
     @Test
     void shouldNormalizeValueWithOneDecimalPlace() {
-        var expectedAmount = new BigDecimal("10.50");
-        
-        Money money = Money.of("10.5");
+        var expectedAmount = new BigDecimal("9.50");
+
+        Money money = Money.of("9.5");
 
         assertThat(money.amount()).isEqualTo(expectedAmount);
     }
+
+    @Test
+    void shouldNormalizeAmountWithTrailingFractionalZeros() {
+        var expectedAmount = new BigDecimal("10.00");
+
+        Money money = Money.of("10.000");
+
+        assertThat(money.amount()).isEqualTo(expectedAmount);
+    }
+
+
 }
