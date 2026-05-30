4ad6ecd93a50e89442a34ab95cc4dafe13569e9e
Jose P. Ferreira <shzexyz@gmail.com>
2026-05-30 18:19:15 -0300

    test(transfer): specify rejection of non-positive amount

--- src/test/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransferTest.java
+++ src/test/java/io/github/phlmth/ledgerpay/domain/transfer/PeerTransferTest.java
@@ -1,6 +1,7 @@
 package io.github.phlmth.ledgerpay.domain.transfer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import io.github.phlmth.ledgerpay.domain.money.Money;
 import io.github.phlmth.ledgerpay.domain.wallet.Wallet;
@@ -19,5 +20,33 @@ class PeerTransferTest {
 
     assertThat(source.balance()).isEqualTo(Money.of("60.00"));
     assertThat(destination.balance()).isEqualTo(Money.of("40.00"));
+  }
+
+  @Test
+  void shouldRejectZeroTransfer() {
+    Wallet source = new Wallet(Money.of("100.00"));
+    Wallet destination = new Wallet(Money.of("20.00"));
+
+    PeerTransfer transfer = new PeerTransfer();
+
+    assertThatThrownBy(() -> transfer.execute(source, destination, Money.of("0.00")))
+        .isInstanceOf(IllegalArgumentException.class);
+
+    assertThat(source.balance()).isEqualTo(Money.of("100.00"));
+    assertThat(destination.balance()).isEqualTo(Money.of("20.00"));
+  }
+
+  @Test
+  void shouldRejectNegativeTransfer() {
+    Wallet source = new Wallet(Money.of("100.00"));
+    Wallet destination = new Wallet(Money.of("20.00"));
+
+    PeerTransfer transfer = new PeerTransfer();
+
+    assertThatThrownBy(() -> transfer.execute(source, destination, Money.of("-10.00")))
+        .isInstanceOf(IllegalArgumentException.class);
+
+    assertThat(source.balance()).isEqualTo(Money.of("100.00"));
+    assertThat(destination.balance()).isEqualTo(Money.of("20.00"));
   }
 }
